51bddbde9327fed1512db938342130a4ee6d7b2d
fatma <fatma@fatma-VirtualBox.(none)>
2014-09-30 19:46:48 +0200

    c

---
+++ Document/Client.php
@@ -0,0 +1,25 @@
+namespace Acme\OAuthServerBundle\Document;  
+  
+use FOS\OAuthServerBundle\Document\Client as BaseClient;  
+  
+class Client extends BaseClient   
+{  
+    protected $id;  
+    protected $name;  
+      
+    public function __construct()  
+    {  
+        parent::__construct();  
+    }  
+      
+    public function getName()  
+    {  
+        return $this->name;  
+    }  
+  
+    public function setName($name)  
+    {  
+        $this->name = $name;  
+    }      
+  
+}  
